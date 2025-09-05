e108522e8561fc357ef88153bf5ff6cbc629e22d
Laurkan <laurkanr@gmail.com>
2025-09-05 11:55:41 -0400

    added a minimal checking server

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+class Checker:
+    responses = {
+        "septicDC": True
+    }
+
+    name_key = "name"
+
+    @classmethod
+    def is_good(cls, data):
+        name = data.get(cls.name_key)
+        if type(name) == str and len(name) > 0:
+            return cls.responses.get(name, None)
+        else:
+            return None
+
+output_key = "result"
+
+@app.route('/', method=['POST'])
+def run():
+    if request.is_json:
+        data = request.json
+        output_dict = {}
+        output_dict[output_key] = Checker.is_good(data)
+        return jsonify(output_dict), 200
+    else:
+        return "Expected JSON input", 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
